86568c99683e24250ec536e06a0aa68149110854
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-18 09:04:05 -0800

    good fail for OrderConstructor test (typeof not equal)

---
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order testOrder = new Order();
+      int number = 8;
+      Assert.AreEqual(typeof(Order), number.GetType());
+    }
+  }
+}
