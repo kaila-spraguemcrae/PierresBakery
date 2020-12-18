8e6d1f6ef92c06048912add198f2e2a8633e68b7
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-18 09:37:04 -0800

    good fail for GetDate test (values return different failes Assert.AreEqual)

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -9,28 +9,35 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order testOrder = new Order("test", "test", 0);
+      Order testOrder = new Order("test", "test", 0, "test");
       Assert.AreEqual(typeof(Order), testOrder.GetType());
     }
     [TestMethod]
     public void GetTitle_ReturnTitle_String()
     {
-      Order testOrder = new Order("Pastry Order", "test", 0);
+      Order testOrder = new Order("Pastry Order", "test", 0, "test");
       Assert.AreEqual("Pastry Order", testOrder.Title);
     }
     [TestMethod]
     public void GetDescription_ReturnDescription_String()
     {
       string description =  "Order of 20 Pastries";
-      Order testOrder = new Order("test", description, 0);
+      Order testOrder = new Order("test", description, 0, "test");
       Assert.AreEqual(description, testOrder.Description);
     }
     [TestMethod]
     public void GetPrice_ReturnPrice_Int()
     {
       int price = 34;
-      Order testOrder = new Order ("test", "test", price);
+      Order testOrder = new Order ("test", "test", price, "test");
       Assert.AreEqual(price, testOrder.Price);
+    }
+    [TestMethod]
+    public void GetDate_ReturnDate_String()
+    {
+      string date = "12.18.2020";
+      Order testOrder = new Order("test", "test", 0, date);
+      Assert.AreEqual(date, testOrder.Date);
     }
   }
 }
--- PierresBakery/Models/Order.cs
+++ PierresBakery/Models/Order.cs
@@ -5,12 +5,14 @@ namespace PierresBakery.Models
     public string Title { get; set; }
     public string Description { get; set; }
     public int Price { get; set; }
+    public string Date { get; set; }
 
-    public Order(string title, string description, int price)
+    public Order(string title, string description, int price, string date)
     {
       Title = title;
       Description = description;
       Price = price;
+      Date = "date";
     }
   }
 }
