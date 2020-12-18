7de94f1a9cbe4fb2b5516becb77d23e70af51a8f
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-18 13:19:50 -0800

    pass Order.Find test

--- PierresBakery/Models/Order.cs
+++ PierresBakery/Models/Order.cs
@@ -27,7 +27,7 @@ namespace PierresBakery.Models
     }
     public static Order Find(int searchId)
     {
-      return _instances[searchId-2];
+      return _instances[searchId-1];
     }
     public static void ClearAll()
     {
