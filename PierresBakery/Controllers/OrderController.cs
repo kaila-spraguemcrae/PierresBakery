3778e0096061946511f94d2cb3a77ea4729987e1
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-18 15:12:54 -0800

    create splash page, vendor index and form

---
+++ PierresBakery/Controllers/OrderController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PierresBakery.Controllers
+{
+  public class OrderController : Controller
+  {
+    
+  }
+} 
---
+++ PierresBakery/Controllers/VendorController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using PierresBakery.Models;
+
+namespace PierresBakery.Controllers
+{
+  public class VendorController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
+  }
+} 
