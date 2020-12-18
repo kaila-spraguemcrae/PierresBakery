65e96bcb2afe86d83f2cff757182b3d11dc6b769
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-18 09:18:30 -0800

    good fail for GetTitle (returns different string values)

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -9,8 +9,14 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order testOrder = new Order();
+      Order testOrder = new Order("test");
       Assert.AreEqual(typeof(Order), testOrder.GetType());
+    }
+    [TestMethod]
+    public void GetTitle_ReturnTitle_String()
+    {
+      Order testOrder = new Order("Pastry Order");
+      Assert.AreEqual("Pastry Order", testOrder.Title);
     }
   }
 }
--- PierresBakery/Models/Order.cs
+++ PierresBakery/Models/Order.cs
@@ -2,6 +2,11 @@ namespace PierresBakery.Models
 {
   public class Order
   {
-    
+    public string Title { get; set; }
+
+    public Order(string title)
+    {
+      Title = "Order";
+    }
   }
 }
