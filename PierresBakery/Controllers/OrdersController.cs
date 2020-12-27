ad87ac6b6163b4cca37b33d876c30c297b6b8feb
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-12-27 12:52:19 -0800

    update views naming and pathways

--- PierresBakery/Controllers/OrdersController.cs
+++ PierresBakery/Controllers/OrdersController.cs
@@ -5,7 +5,7 @@ using PierresBakery.Models;
 
 namespace PierresBakery.Controllers
 {
-  public class OrderController : Controller
+  public class OrdersController : Controller
   {
     [HttpGet("/vendors/{vendorId}/orders/new")]
     public ActionResult New(int vendorId)
--- PierresBakery/Controllers/VendorsController.cs
+++ PierresBakery/Controllers/VendorsController.cs
@@ -5,7 +5,7 @@ using PierresBakery.Models;
 
 namespace PierresBakery.Controllers
 {
-  public class VendorController : Controller
+  public class VendorsController : Controller
   {
     [HttpGet("/vendors")]
     public ActionResult Index()
